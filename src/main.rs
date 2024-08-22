efa0397eeecf93714cfe87816cefb9fb2d2335c6
neruneruna7 <neruneruna7@gmail.com>
2024-08-22 16:42:04 +0900

    とても簡易な構文解析をしてみる

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use prac_compiler::scan::scan3;
 
-mod parser {
-    mod parser1 {
+pub mod parser {
+    pub mod parser1 {
         use crate::scan3::{self, Kind, Lexer, Token};
 
         pub struct Parser<'a> {
@@ -53,46 +53,12 @@ mod parser {
 }
 
 fn main() {
-    println!("Hello, world!");
-    let source = "
-    if + {comment} if /* comment */
-    a + b
-    1+1
-    if true then
-        1 + 1
-    else
-        4 - 3
-    ";
-
-    let source = "
-    if true then
-    if if if if if if
-    c c c c c c c c 
-    { this is a comment }
-        1 + 1
-    else
-        i := 5
-    ";
-
     let source = "
     program sample; .";
 
     let mut lexer = scan3::Lexer::new(source);
-    let tokens = lexer.analyze();
+    let mut parser = parser::parser1::Parser::new(lexer);
+    parser.parse_program();
 
-    // for i in tokens.iter() {
-    //     println!("{:?}", i);
-    // }
-
-    // 字句の出現数をカウント
-    let mut count = std::collections::HashMap::new();
-    for token in tokens.into_iter() {
-        let token = (token.kind, token.value);
-        let entry = count.entry(token).or_insert(0);
-        *entry += 1;
-    }
-
-    for (token, count) in count.iter() {
-        println!("{:?} : {}", token, count);
-    }
+    println!("parsing OK \n{}", source);
 }
