945248187ed339257a518eb0b14ae3d5098a9a22
neruneruna7 <neruneruna7@gmail.com>
2024-08-22 17:07:33 +0900

    add: syntax error出力用の関数を切り出し

--- src/main.rs
+++ src/main.rs
@@ -28,10 +28,13 @@ pub mod parser {
                             return;
                         }
                     }
-                    None => {
-                    }
+                    None => {}
                 }
 
+                self.syntax_error();
+            }
+
+            fn syntax_error(&self) {
                 let sliced_source = if let Some(ref l) = self.lookahead {
                     &self.lexer.source[..l.start]
                 } else {
@@ -40,11 +43,14 @@ pub mod parser {
 
                 panic!(
                     "syntax error source code:\n\n {} \n\n expect {:?} but found {:?}",
-                    sliced_source, kind, &self.lookahead
+                    sliced_source,
+                    Kind::Eof,
+                    &self.lookahead
                 );
             }
 
-            // "program" "名前" ";" ブロック "."
+            /// パースの開始
+            /// "program" "名前" ";" ブロック "."
             pub fn parse_program(&mut self) {
                 // マクロ構文のprogramに該当
                 self.match_token(Kind::Program);
@@ -54,8 +60,36 @@ pub mod parser {
                 self.match_token(Kind::Dot);
             }
 
-            // { 変数宣言部 | 副プログラム宣言 } 複合文
+            /// { 変数宣言部 | 副プログラム宣言 } 複合文
             fn block(&mut self) {}
+
+            /// "var" 変数名の並び ":" 型 ";" { 変数名の並び ":" 型 ";" }
+            fn variable_declaration_part(&mut self) {}
+
+            /// 変数名 { "," 変数名 }
+            fn variable_names(&mut self) {}
+
+            /// "名前"
+            fn valriable_name(&mut self) {
+                self.match_token(Kind::Name);
+            }
+
+            /// 標準型 | 配列型
+            /// 予約語に引っかかるのを防ぐため，アンダーバーをつけている
+            fn type_(&mut self) {}
+
+            /// "integer" | "boolean" | "char"
+            fn standard_type(&mut self) {
+                match self.lookahead {
+                    Some(ref l) => match l.kind {
+                        Kind::Integer => self.match_token(Kind::Integer),
+                        Kind::Boolean => self.match_token(Kind::Boolean),
+                        Kind::Char => self.match_token(Kind::Char),
+                        _ => todo!(),
+                    },
+                    _ => todo!(),
+                }
+            }
         }
     }
 }
