b2a7f0d00273766bcff0d39f0c4c46381b75e25b
neruneruna7 <neruneruna7@gmail.com>
2024-08-30 19:05:39 +0900

    update: first集合を求めるバイナリ

---
+++ src/bin/first.rs
@@ -0,0 +1,169 @@
+// EBNFからFIRST集合を求めるプログラム
+// []は1回または0回
+// {}は0回以上の繰り返し
+// |はまたは
+// ->は導出
+// ::=は定義
+// EBNFの例
+// <A> ::= <B> | <C> | <D>
+// <B> ::= "b"
+// <C> ::= [ "c" ] "b"
+// <D> ::= { "d" } "b"
+use std::{
+    collections::{HashMap, HashSet},
+    sync::OnceLock,
+};
+
+#[derive(Debug, Clone)]
+struct Rule {
+    left: String,
+    right: String,
+}
+
+struct FirstSet {
+    first: Vec<String>,
+}
+
+fn main() {
+    let rules = [
+        Rule {
+            left: "A".to_string(),
+            right: "B".to_string(),
+        },
+        Rule {
+            left: "A".to_string(),
+            right: "C".to_string(),
+        },
+        Rule {
+            left: "A".to_string(),
+            right: "D".to_string(),
+        },
+        Rule {
+            left: "B".to_string(),
+            right: "b".to_string(),
+        },
+        Rule {
+            left: "C".to_string(),
+            right: "[ c ] b".to_string(),
+        },
+        Rule {
+            left: "D".to_string(),
+            right: "{ d } b".to_string(),
+        },
+    ];
+
+    let first_set = calc_first_set(rules.to_vec());
+    for (k, v) in first_set.iter() {
+        println!("FIRST({}) = {:?}", k, v);
+    }
+}
+
+// Once_cellで1度だけ書き換え可能な文法規則のベクタ
+static RULES: OnceLock<Vec<Rule>> = OnceLock::new();
+
+fn calc_first_set(rules: Vec<Rule>) -> HashMap<String, HashSet<String>> {
+    // rulesをOnce_cellでグローバル変数に代入する
+    RULES.get_or_init(|| rules);
+
+    let mut first_set = HashMap::new();
+    // 文法規則すべてについて，FIRST集合を求める
+    for r in RULES.get().unwrap().iter() {
+        let set = first(&r.left);
+        first_set.insert(r.left.clone(), set);
+    }
+
+    first_set
+}
+
+fn first(a: &str) -> HashSet<String> {
+    // 空白で区切る
+    let a = a.split_whitespace().collect::<Vec<&str>>();
+    // 0 FIRST(a)を空集合に初期化
+    let mut first_a = HashSet::new();
+
+    // 以下のステップを，付け加えるものがなくなるまで繰り返す
+    loop {
+        let before = first_a.clone();
+        match a.as_slice() {
+            // 1 aが空系列ならFIRST(a)に空系列を追加
+            ["ε"] => {
+                first_a.insert("ε".to_string());
+            }
+            // 2 aが終端記号1文字なら，FIRST(a)にその終端記号を追加
+            _ if a.len() == 1 && is_terminal(&a[0]) => {
+                first_a.insert(a[0].to_string());
+            }
+            // 3 aが非終端記号A1文字なら，Aを左辺にもつすべての生成規則についてのFIRST集合を求め，
+            // その要素をFIRST(A)に付け加える
+            _ if a.len() == 1 && !is_terminal(&a[0]) => {
+                let a = a[0];
+                let rules = RULES.get().unwrap();
+                for r in rules.iter() {
+                    if r.left == a {
+                        let set = first(&r.right);
+                        first_a.extend(set);
+                    }
+                }
+            }
+            // 5 aが b | y の場合 FIRST(b)とFIRST(y)をFIRST(a)に付け加える
+            [b, "|", y] => {
+                let set_b = first(b);
+                let set_y = first(y);
+                first_a.extend(set_b);
+                first_a.extend(set_y);
+            }
+            // 6 aが{ b } の場合 FIRST(b)とεをFIRST(a)に付け加える
+            ["{", b, "}"] => {
+                let set_b = first(b);
+                first_a.extend(set_b);
+                first_a.insert("ε".to_string());
+            }
+            // 7 aが[ b ] の場合 FIRST(b)とεをFIRST(a)に付け加える
+            ["[", b, "]"] => {
+                let set_b = first(b);
+                first_a.extend(set_b);
+                first_a.insert("ε".to_string());
+            }
+            // 8 aが( b )の場合 FIRST(b)をFIRST(a)に付け加える
+            ["(", b, ")"] => {
+                let set_b = first(b);
+                first_a.extend(set_b);
+            }
+            // 4 aが長さ2以上の系列の場合(a=Xbとする)
+            _ if a.len() >= 2 => {
+                // 4-1 FIRST(X)がεを含まないなら，FIRST(X)をFIRST(a)に付け加える
+                let x = a[0];
+                let mut set_x = first(x);
+                if !set_x.contains("ε") {
+                    first_a.extend(set_x);
+                }
+                // 4-1 FIRST(X)がεを含むなら，FIRST(X)からεを取り除いたものとFIRST(b)をFIRST(a)に付け加える
+                else {
+                    set_x.remove("ε");
+                    first_a.extend(set_x);
+                    let b = a[1];
+                    let set_b = first(b);
+                    first_a.extend(set_b);
+                }
+            }
+
+            _ => {
+                eprintln!("error first set fn");
+            }
+        }
+
+        // 付け加えるものがなくなったら終了
+        if before == first_a {
+            break;
+        }
+    }
+
+    first_a
+}
+
+// 文字列が終端記号かどうか
+// すべて小文字なら終端記号
+// キャメルケースは非終端記号
+fn is_terminal(s: &str) -> bool {
+    s.chars().all(|c| c.is_lowercase())
+}
