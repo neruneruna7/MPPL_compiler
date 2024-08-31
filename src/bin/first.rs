ca30575da6e7333ae5103ebac3b45852e9dd0ed1
neruneruna7 <neruneruna7@gmail.com>
2024-08-31 14:08:03 +0900

    add: パターン4と5のテスト追加 不具合修正

--- src/bin/first.rs
+++ src/bin/first.rs
@@ -85,78 +85,60 @@ fn first(a: &[&str]) -> HashSet<String> {
     // 以下のステップを，付け加えるものがなくなるまで繰り返す
     loop {
         let before = first_a.clone();
-        match a {
-            // 1 aが空系列ならFIRST(a)に空系列を追加
-            ["ε"] => {
-                first_a.insert("ε".to_string());
-            }
-            // 2 aが終端記号1文字なら，FIRST(a)にその終端記号を追加
-            _ if a.len() == 1 && is_terminal(&a[0]) => {
-                first_a.insert(a[0].to_string());
-            }
-            // 3 aが非終端記号A1文字なら，Aを左辺にもつすべての生成規則についてのFIRST集合を求め，
-            // その要素をFIRST(A)に付け加える
-            _ if a.len() == 1 && !is_terminal(&a[0]) => {
-                let a = a[0];
-                let rules = RULES.get().unwrap();
-                for r in rules.iter() {
-                    if r.left == a {
-                        let right = r.right.split_whitespace().collect::<Vec<&str>>();
-                        let set = first(&right);
-                        first_a.extend(set);
-                    }
+        // 1 aが空系列ならFIRST(a)に空系列を追加
+        if a == &["ε"] {
+            first_a.insert("ε".to_string());
+        }
+        // 2 aが終端記号1文字なら，FIRST(a)にその終端記号を追加
+        if a.len() == 1 && is_terminal(&a[0]) {
+            first_a.insert(a[0].to_string());
+        }
+        // 3 aが非終端記号A1文字なら，Aを左辺にもつすべての生成規則についてのFIRST集合を求め，
+        // その要素をFIRST(A)に付け加える
+        if a.len() == 1 && !is_terminal(&a[0]) {
+            let rules = RULES.get().unwrap();
+            for r in rules.iter() {
+                if r.left == a[0] {
+                    let right = r.right.split_whitespace().collect::<Vec<&str>>();
+                    let set = first(&right);
+                    first_a.extend(set);
                 }
             }
-            // 5 aが b | y の場合 FIRST(b)とFIRST(y)をFIRST(a)に付け加える
-            _ if matches!(is_pattern5(a), Some(b)) => {
-                let (b, y) = is_pattern5(a).unwrap();
-                let set_b = first(&b);
-                let set_y = first(&y);
+        }
+        // 5 aが b | y の場合 FIRST(b)とFIRST(y)をFIRST(a)に付け加える
+        if let Some((b, y)) = is_pattern5(a) {
+            let set_b = first(&b);
+            let set_y = first(&y);
+            first_a.extend(set_b);
+            first_a.extend(set_y);
+        }
+        // 6 aが{ b } の場合 FIRST(b)とεをFIRST(a)に付け加える
+        if let Some(b) = is_pattern6(a) {
+            let set_b = first(&b);
+            first_a.extend(set_b);
+            first_a.insert("ε".to_string());
+        }
+        // 7 aが[ b ] の場合 FIRST(b)とεをFIRST(a)に付け加える
+        if let Some(b) = is_pattern7(a) {
+            let set_b = first(&b);
+            first_a.extend(set_b);
+            first_a.insert("ε".to_string());
+        }
+        // 4 aが長さ2以上の系列の場合(a=Xbとする)
+        if a.len() >= 2 {
+            // 4-1 FIRST(X)がεを含まないなら，FIRST(X)をFIRST(a)に付け加える
+            let x = a[0];
+            let mut set_x = first(&[x]);
+            if !set_x.contains("ε") {
+                first_a.extend(set_x);
+            }
+            // 4-1 FIRST(X)がεを含むなら，FIRST(X)からεを取り除いたものとFIRST(b)をFIRST(a)に付け加える
+            else {
+                set_x.remove("ε");
+                first_a.extend(set_x);
+                let b = a[1];
+                let set_b = first(&[b]);
                 first_a.extend(set_b);
-                first_a.extend(set_y);
-            }
-            // 6 aが{ b } の場合 FIRST(b)とεをFIRST(a)に付け加える
-            _ if matches!(is_pattern6(a), Some(b)) => {
-                // matches! でとった変数のスコープは，ここにまでは持ってこれないっぽい？
-                // ので，仕方なくここでもう一度関数を呼ぶ
-                let b = is_pattern6(a).unwrap();
-                let set_b = first(&b);
-                first_a.extend(set_b);
-                first_a.insert("ε".to_string());
-            }
-            // 7 aが[ b ] の場合 FIRST(b)とεをFIRST(a)に付け加える
-            _ if matches!(is_pattern7(a), Some(b)) => {
-                let b = is_pattern7(a).unwrap();
-                let set_b = first(&b);
-                first_a.extend(set_b);
-                first_a.insert("ε".to_string());
-            }
-            // 8 aが( b )の場合 FIRST(b)をFIRST(a)に付け加える
-            _ if matches!(is_pattern8(a), Some(b)) => {
-                let b = is_pattern8(a).unwrap();
-                let set_b = first(&b);
-                first_a.extend(set_b);
-            }
-            // 4 aが長さ2以上の系列の場合(a=Xbとする)
-            _ if a.len() >= 2 => {
-                // 4-1 FIRST(X)がεを含まないなら，FIRST(X)をFIRST(a)に付け加える
-                let x = a[0];
-                let mut set_x = first(&[x]);
-                if !set_x.contains("ε") {
-                    first_a.extend(set_x);
-                }
-                // 4-1 FIRST(X)がεを含むなら，FIRST(X)からεを取り除いたものとFIRST(b)をFIRST(a)に付け加える
-                else {
-                    set_x.remove("ε");
-                    first_a.extend(set_x);
-                    let b = a[1];
-                    let set_b = first(&[b]);
-                    first_a.extend(set_b);
-                }
-            }
-
-            _ => {
-                eprintln!("error first set fn");
             }
         }
 
@@ -167,6 +149,66 @@ fn first(a: &[&str]) -> HashSet<String> {
     }
 
     first_a
+}
+
+// 2つの系列に分けたい
+// 分けられないならNone
+// a B
+// (a | b) B
+// [ a ] B
+// { a } B
+fn is_pattern4<'a>(a: &[&'a str]) -> Option<(Vec<&'a str>, Vec<&'a str>)> {
+    // 長さが1以下なら分けられない
+    if a.len() < 2 {
+        return None;
+    }
+    let mut map = HashMap::new();
+    map.insert("(", 0);
+    map.insert("[", 0);
+    map.insert("{", 0);
+    let mut left = Vec::new();
+    for i in a.iter() {
+        match *i {
+            "(" => {
+                let c = map.get_mut(&"(").unwrap();
+                *c += 1;
+            }
+            ")" => {
+                let c = map.get_mut(&"(").unwrap();
+                *c -= 1;
+            }
+            "{" => {
+                let c = map.get_mut(&"{").unwrap();
+                *c += 1;
+            }
+            "}" => {
+                let c = map.get_mut(&"{").unwrap();
+                *c -= 1;
+            }
+            "[" => {
+                let c = map.get_mut(&"[").unwrap();
+                *c += 1;
+            }
+            "]" => {
+                let c = map.get_mut(&"[").unwrap();
+                *c -= 1;
+            }
+            _ => {
+                // HashMapのvalueがすべて0なら分けられる
+                if map.values().all(|x| *x == 0) {
+                    // a B などの場合，leftにaが入っているべきだが，まだ入っていないままここに来る
+                    // そのため，leftが空なら，leftに要素を入れる
+                    if left.is_empty() {
+                        left.push(*i);
+                    }
+                    let right = a[left.len()..].as_ref();
+                    return Some((left, right.to_vec()));                    
+                }
+            }
+        }
+        left.push(*i);
+    }
+    None
 }
 
 // とにかく，はじめの|の部分とそれ以降の部分に分ける
@@ -208,18 +250,16 @@ fn is_pattern5<'a>(a: &[&'a str]) -> Option<(Vec<&'a str>, Vec<&'a str>)> {
                 let c = symbol_count.get_mut(&"[").unwrap();
                 *c -= 1;
             }
-            "|" => {
-                let right = a[left.len()..].as_ref();
+            "|" if symbol_count.values().all(|x| *x == 0) => {
+                let right = a[left.len()+1..].as_ref();
                 return Some((left, right.to_vec()));
             }
             _ => {
-                unreachable!()
             }
         }
         left.push(*i);
     }
-    // ( { a } | b | [ c ] ) { a }
-    todo!()
+    None
 }
 
 fn is_pattern6<'a>(a: &[&'a str]) -> Option<Vec<&'a str>> {
@@ -281,4 +321,98 @@ fn is_pattern8<'a>(a: &[&'a str]) -> Option<Vec<&'a str>> {
 // キャメルケースは非終端記号
 fn is_terminal(s: &str) -> bool {
     s.chars().all(|c| c.is_lowercase())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // #[test]
+    // fn test_first() {
+    //     let rules = [
+    //         Rule {
+    //             left: "A".to_string(),
+    //             right: "B".to_string(),
+    //         },
+    //         Rule {
+    //             left: "A".to_string(),
+    //             right: "C".to_string(),
+    //         },
+    //         Rule {
+    //             left: "A".to_string(),
+    //             right: "D".to_string(),
+    //         },
+    //         Rule {
+    //             left: "B".to_string(),
+    //             right: "d".to_string(),
+    //         },
+    //         Rule {
+    //             left: "C".to_string(),
+    //             right: "[ c ] b".to_string(),
+    //         },
+    //         Rule {
+    //             left: "D".to_string(),
+    //             right: "{ d } b".to_string(),
+    //         },
+    //     ];
+
+    //     let first_set = calc_first_set(rules.to_vec());
+    //     assert_eq!(first_set.get("A").unwrap(), &["d", "c", "ε"].iter().map(|x| x.to_string()).collect());
+    //     assert_eq!(first_set.get("B").unwrap(), &["d"].iter().map(|x| x.to_string()).collect());
+    //     assert_eq!(first_set.get("C").unwrap(), &["c", "b", "ε"].iter().map(|x| x.to_string()).collect());
+    //     assert_eq!(first_set.get("D").unwrap(), &["d", "b", "ε"].iter().map(|x| x.to_string()).collect());
+    // }
+
+    #[test]
+    fn test_is_terminal() {
+        assert_eq!(is_terminal("a"), true);
+        assert_eq!(is_terminal("A"), false);
+        assert_eq!(is_terminal("abc"), true);
+        assert_eq!(is_terminal("Abc"), false);
+    }
+
+    #[test]
+    fn test_is_pattern4() {
+        let a = ["a", "B"];
+        let (b, y) = is_pattern4(&a).unwrap();
+        assert_eq!(b, ["a"]);
+        assert_eq!(y, ["B"]);
+
+        let a = ["(", "a", "|", "b", ")", "B"];
+        let (b, y) = is_pattern4(&a).unwrap();
+        assert_eq!(b, ["(", "a", "|", "b", ")"]);
+        assert_eq!(y, ["B"]);
+
+        let a = ["[", "a", "]", "B"];
+        let (b, y) = is_pattern4(&a).unwrap();
+        assert_eq!(b, ["[", "a", "]"]);
+        assert_eq!(y, ["B"]);
+
+        let a = ["{", "a", "}", "B"];
+        let (b, y) = is_pattern4(&a).unwrap();
+        assert_eq!(b, ["{", "a", "}"]);
+        assert_eq!(y, ["B"]);
+    }
+
+    #[test]
+    fn test_is_pattern5() {
+        let a = ["a", "|", "b"];
+        let (b, y) = is_pattern5(&a).unwrap();
+        assert_eq!(b, ["a"]);
+        assert_eq!(y, ["b"]);
+
+        let a = ["(", "a", "|", "b", ")", "B"];
+        let r = is_pattern5(&a);
+        assert_eq!(r, None);
+
+        let a = ["[", "a", "]","|",  "B"];
+        let (b, y) = is_pattern5(&a).unwrap();
+        assert_eq!(b, ["[", "a", "]"]);
+        assert_eq!(y, ["B"]);
+
+        let a = ["{", "a", "}","|", "B"];
+        let (b, y) = is_pattern5(&a).unwrap();
+        assert_eq!(b, ["{", "a", "}"]);
+        assert_eq!(y, ["B"]);
+    }
 }
